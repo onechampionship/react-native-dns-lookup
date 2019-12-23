d61c135df3828077ce4beaae80178f7cfe59bb82
mahendra0203 <mahendra0203@gmail.com>
2019-12-23 15:11:03 +0530

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 declare module 'react-native-dns-lookup' {
   export function getIpAddressesForHostname(hostname: string): Promise<ReadonlyArray<string>>;
+  export function getCNameForHostname(hostname: string): Promise<Readonly<string>>;
 }
